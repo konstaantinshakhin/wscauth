a70b3ae7c022b8293293043fd0a5e52c3677cf0e
shakhin.k <k.shakhin@yandex.ru>
2015-10-08 13:59:31 +0300

    gghjfgfg

--- src/main/java/ru/shakhin/wscauth/WebSocketAuth.java
+++ src/main/java/ru/shakhin/wscauth/WebSocketAuth.java
@@ -12,24 +12,31 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint(value = "/websocketauthendpoint")
+@ServerEndpoint(value = "/websocketauthendpoint", encoders = { MessageEncoder.class }, decoders = { MessageDecoder.class })
 public class WebSocketAuth {
     private static Set<Session> peers = Collections.synchronizedSet(new HashSet<Session>());
 
     @OnOpen
     public void onOpen(Session peer) {
-        peers.add(peer);
+
+            System.out.println("Client connected");
+            peers.add(peer);
     }
 
     @OnClose
     public void onClose(Session peer) {
+        System.out.println("Connection closed");
         peers.remove(peer);
     }
 
     @OnMessage
-    public void onMessage(String message, Session session) throws IOException, EncodeException {
+    public void onMessage(Message message, Session session) throws IOException, EncodeException {
         System.out.println("message: " + message);
+        Message response = new Message();
+        response.setSubject("Response to " + message.getSubject());
+        response.setContent("echo " + message.getContent());
+        session.getBasicRemote().sendObject(response);
 
-                session.getBasicRemote().sendObject(message);
+        // session.getBasicRemote().sendObject(message);
         }
     }
